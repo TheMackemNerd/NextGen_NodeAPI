76ebc40cb62f2a5ba94f65cc9644d4fa7115b9d5
John Ferguson <John.Ferguson@IRIS.CO.UK>
2019-06-20 13:56:20 +0100

    add user to user pool

--- NextGen_NodeAPI/server.js
+++ NextGen_NodeAPI/server.js
@@ -1,6 +1,8 @@
 'use strict';
 var http = require('http');
 var AWS = require("aws-sdk")
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js')
+const CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -8,6 +10,10 @@ const app = express()
 
 var sub = "";
 var id = 0;
+var email = "";
+var fullname = "";
+var tenant = "";
+
 
 AWS.config.update({ endpoint: "https://dynamodb.eu-west-1.amazonaws.com" });
 
@@ -77,16 +83,56 @@ app.get('/api/v1/users', cors(), function(req, res, next) {
 app.post('/api/v1/users', cors(), function (req, res, next) {
 
     console.log("Getting body values");
-    console.log("Body: " + JSON.stringify(req.body));
-    var email = req.body.emailaddress;
-    var fullname = req.body.fullname;
-    var tenant = req.body.tenant;
+    email = req.body.emailaddress;
+    fullname = req.body.fullname;
+    tenant = req.body.tenant;
 
     console.log(email + " " + fullname + " " + tenant);
-    
-    res.status(200).send();
+
+    addUserToCognito(function (error, item) {
+        if (error) {
+            console.log("Couldn't add user to Cognito");
+            console.log(error);
+            outputError(res, 400, "5", "Error adding user to the directory", error.desc);
+        }
+        else {
+            console.log("User added successfully");
+            res.status(200).send("{ 'sub' : '" + item + "'}");
+        }
+
+    });   
 
 });
+
+function addUserToCognito(callback) {
+
+    const poolData = {
+        UserPoolId: "eu-west-1_2DtCcoypN",    
+        ClientId: "57vo0lcv2gq0822td26v9nhnh6" // Your client id here
+    }; 
+
+    const pool_region = 'eu-west-1';
+
+    const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+    var attributeList = [];
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({ Name: "email", Value: email }));
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({ Name: "custom:tenant", Value: tenant }));
+
+    userPool.signUp(email, 'SamplePassword123', attributeList, null, function (err, result) {
+        if (err) {
+            console.log(err);
+            callback(err);
+        }
+
+        var cognitoUser = result.user;
+        console.log('user name is ' + cognitoUser.getUsername());
+    });
+
+    callback(null,cognitoUser.getUsername());
+
+}
+
 
 
 function outputError(res, status, code, short, desc) {
@@ -94,7 +140,7 @@ function outputError(res, status, code, short, desc) {
     var body = {
         code: code,
         short: short,
-        description: desc
+        message: desc
     };
 
     res.status(status).send(JSON.stringify(body));
