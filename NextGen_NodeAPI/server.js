1d59c407150fc49f64bb174cb5b63e33512f39fa
John Ferguson <John.Ferguson@IRIS.CO.UK>
2019-07-01 10:34:10 +0100

    read body

--- NextGen_NodeAPI/server.js
+++ NextGen_NodeAPI/server.js
@@ -94,7 +94,14 @@ app.put('/api/v1/users/me/mfa', cors(), function (req, res, next) {
     sub = req.header("X-USER");
     console.log("X-USER: " + sub);
 
-    var bod = JSON.stringify(req.body);
+    let body = [];
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+    });
+    console.log(body);
+    var bod = JSON.parse(req.body);
     console.log(bod);
     var mfa_enabled = bod.mfa_enabled;
 
