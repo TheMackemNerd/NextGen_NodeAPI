7ab89596644dc7035c4a70fddd666ef31d6c127d
John Ferguson <John.Ferguson@IRIS.CO.UK>
2020-04-01 11:28:29 +0100

    api changes

--- NextGen_NodeAPI/server.js
+++ NextGen_NodeAPI/server.js
@@ -50,6 +50,31 @@ app.use(function (req, res, next) {
 
 app.use(cors());
 
+app.get('/api/v1/widgets', function (req, res, next) {
+    res.status(200).json([{ id: 1, name: 'foo', type: 'Standard' }, { id: 2, name: 'bar', type: 'Standard' }]);
+});
+
+app.post('/api/v1/widgets', function (req, res, next) {
+    res.status(200).send();
+});
+
+app.delete('/api/v1/widgets', function (req, res, next) {
+    res.status(200).send();
+});
+
+app.get('/api/v1/widgets/1', function (req, res, next) {
+    res.status(200).json({ id: 1, name: 'foo', type: 'Standard' });
+});
+
+app.put('/api/v1/widgets/1', function (req, res, next) {
+    res.status(200).send();
+});
+
+app.delete('/api/v1/widgets/1', function (req, res, next) {
+    res.status(200).send();
+});
+
+
 app.get('/api/v1/helloworld', function (req, res, next) {
 
     var accesstoken = req.header("Authorization");
