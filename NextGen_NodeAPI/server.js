78eca2a34f1ae5f18faa51162b69c08e3837d308
John Ferguson <John.Ferguson@IRIS.CO.UK>
2021-03-18 08:57:50 +0000

    Update package json

--- NextGen_NodeAPI/server.js
+++ NextGen_NodeAPI/server.js
@@ -3,6 +3,18 @@ const express = require('express');
 const cors = require('cors');
 const app = express();                 
 
+app.use(function (req, res, next) {
+    /*
+    console.log("Setting headers");
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader("Access-Control-Allow-Origin", '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-USER,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    */
+    next();
+})
+
 app.use(cors());
 
 app.get('/api/v1/widgets', function (req, res, next) {
